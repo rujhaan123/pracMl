4b856443f7cbf1ff8f1860274f09f7f3e91d2b05
Rujhaan Bhatia <33410430+rujhaan123@users.noreply.github.com>
2023-10-05 17:40:57 +0530

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -1,37 +1,33 @@
 # Import libraries
-
 import argparse
 import glob
 import os
 import mlflow
 import pandas as pd
-import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-# define functions
+# Define functions
 def main(args):
     # TO DO: enable autologging
     mlflow.autolog()
 
-    # read data
+    # Read data
     df = get_csvs_df(args.training_data)
 
-    # split data
+    # Split data
     X_train, X_test, y_train, y_test = split_data(df)
 
-    # train model
+    # Train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
-
 
 def get_csvs_df(path):
     if not os.path.exists(path):
-        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
+        raise RuntimeError(f"Cannot use a non-existent path provided: {path}")
     csv_files = glob.glob(f"{path}/*.csv")
     if not csv_files:
-        raise RuntimeError(f"No CSV files found in provided data path: {path}")
+        raise RuntimeError(f"No CSV files found in the provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
-
 
 # TO DO: add function to split data
 def split_data(df):
@@ -39,10 +35,9 @@ def split_data(df):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
     return X_train, X_test, y_train, y_test
 
-
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
-    # train model
-    model=LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    # Train model
+    model = LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
     # Evaluate model
     score = model.score(X_test, y_test)
@@ -50,36 +45,34 @@ def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # Log model and metrics
     mlflow.sklearn.log_model(model, "model")
     mlflow.log_metric("score", score)
-    return model,score
+    return model, score
 
 def parse_args():
-    # setup arg parser
+    # Setup arg parser
     parser = argparse.ArgumentParser()
 
-    # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
+    # Add arguments
+    parser.add_argument("--training_data", dest='training_data', type=str)
+    parser.add_argument("--reg_rate", dest='reg_rate', type=float, default=0.01)
 
-    # parse args
+    # Parse args
     args = parser.parse_args()
 
-    # return args
+    # Return args
     return args
 
-# run script
+# Run script
 if __name__ == "__main__":
-    # add space in logs
+    # Add space in logs
     print("\n\n")
     print("*" * 60)
 
-    # parse args
+    # Parse args
     args = parse_args()
 
-    # run main function
+    # Run main function
     main(args)
 
-    # add space in logs
+    # Add space in logs
     print("*" * 60)
     print("\n\n")
